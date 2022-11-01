dcf843a4c8ce603f0f8327f00f4a6be11c0e4fdc
juribyaan <sekb00@gmail.com>
2022-11-01 16:50:20 +0900

    2

--- python_ex/1101_maze_v1.py
+++ python_ex/1101_maze_v1.py
@@ -10,7 +10,6 @@ import os
 # o o o o x o o o x
 # S x x o o x x o x)
 # f.close()
-
 f1 = open("/home/juribyaab/catkin_ws/maze.txt" , 'r')
 lines = f1.readlines()
 f1.close()
@@ -20,6 +19,7 @@ def main():
     X_point = len(lines)
     line = " ".join(lines)
     list_line = line.split(" ")
+    Y_point = round(len(list_line)/X_point)
     set_play = list_line.index('S')
     list_line.pop(set_play)
     list_line.insert(set_play,'P')
@@ -27,19 +27,19 @@ def main():
     while True:  
         os.system('clear')
         for i in range(X_point):
-            print(" ".join(list_line[i*X_point:(i+1)*X_point]) ,end="" )                    
+            print(" ".join(list_line[i*Y_point:(i+1)*X_point]) ,end="" )
         print("어디로 갈까?")
-        print("1. 동 쪽으로 2. 서 쪽으로 3. 남 쪽으로 4. 북 쪽으로")        
+        print("1. 동 쪽으로 2. 서 쪽으로 3. 남 쪽으로 4. 북 쪽으로")
         x = int(input())
         set_play = list_line.index('P')
         if   x == 1:
-            set_play +=  1    
+            set_play +=  1
         elif x == 2:
             set_play += -1
         elif x == 3:
-            set_play +=  X_point
+            set_play +=  Y_point
         elif x == 4:
-            set_play += -X_point
+            set_play += -Y_point
         try:
             wall = list_line[set_play]
             if wall == "o" or wall == "S":
