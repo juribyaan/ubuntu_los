b615348b9c561150ec6558e30e2b17c637deabdf
Jubibyaan <sekb00@gmail.com>
2022-11-17 09:44:46 +0900

    11111111

---
+++ 1114/1114_1.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int red_F = 80;
+int yellow_F = 50;
+int puple_F =36;
+int min = 5;
+int max = 2000;
+// int r = round(max/red_F);
+int r = max/red_F;
+// int y = round(max/yellow_F);
+// int p = round(max/puple_F);
+
+
+int main(int argc, char const *argv[])
+{
+    int printf(r)
+    return 0;
+}
